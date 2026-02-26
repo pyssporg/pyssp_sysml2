bff5e83fe811eaa41ac8f110db08dc06ed7b14e6
Erik Rosenlund <erik.rosenlund@saabgroup.com>
2026-02-26 11:53:04 +0100

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pyssp_sysml2
+
+Module for handling and working with sysml v2 and ssp 
+
+
---
+++ src/pyssp_sysml2/__init__.py
@@ -0,0 +1,4 @@
+"""SysML utilities package for architecture parsing and generation SSP."""
+
+__version__ = "0.1.0"
+
